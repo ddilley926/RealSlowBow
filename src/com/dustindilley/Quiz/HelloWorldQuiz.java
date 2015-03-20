fea4ba6b917026fcf460bb2a7e41a3af74683b22
ddilley926 <dustin.diolley@hotmail.com>
2015-03-19 20:03:44 -0700

    My quiz answers

--- src/com/dustindilley/Quiz/HelloWorldQuiz.java
+++ src/com/dustindilley/Quiz/HelloWorldQuiz.java
@@ -1,5 +1,34 @@
 package com.dustindilley.Quiz;
 
-public class HelloWorldQuiz {
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class HelloWorldQuiz extends JavaPlugin{
+
+	
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+	if(cmd.getName().equalsIgnoreCase("hello"))	{
+		Player p = (Player) sender;
+		p.sendMessage(ChatColor.RED + "Braden Smells");
+		
+			
+	}
+		
+		return false;
+
+		
+
+
+
+
+
+
+
+
+
+	}
 }
