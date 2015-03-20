e1e549b2b015a20edca62973a8d45d83ae689a7d
ddilley926 <dustin.diolley@hotmail.com>
2015-03-19 18:29:57 -0700

    Addeded Healing Plugin to a new package

---
+++ src/com/dustindilley/HealingPlugin/Main.java
@@ -0,0 +1,36 @@
+
+
+
+
+	package com.dustindilley.Healing;
+
+	import org.bukkit.ChatColor;
+	import org.bukkit.command.Command;
+	import org.bukkit.command.CommandSender;
+	import org.bukkit.entity.Player;
+	import org.bukkit.plugin.java.JavaPlugin;
+
+	public class Main extends JavaPlugin{
+
+
+	public void onEnable(){
+		}
+
+
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		
+	
+		if(cmd.getName().equalsIgnoreCase("speak")){
+		Player p = (Player) sender;
+		
+		p.setHealth("20");	
+		}
+		return false;
+		
+		
+		}
+
+
+
+
+}
