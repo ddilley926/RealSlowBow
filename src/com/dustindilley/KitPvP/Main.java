c4bc829edf0181c922a59fa3f0b8886550269b7a
bradencohen <braden.cohen@yahoo.com>
2015-03-19 22:49:17 -0700

    KitPvP

--- src/com/dustindilley/KitPvP/Main.java
+++ src/com/dustindilley/KitPvP/Main.java
@@ -3,22 +3,11 @@ package com.dustindilley.KitPvP;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
-
-
-
-
-
-
 public void onEnable(){
-	
 	commands();
-	
-	
-	
 }
 
 public void commands(){
-	getCommand("Standard").setExecutor(new Standard ());
 	
 }
 
