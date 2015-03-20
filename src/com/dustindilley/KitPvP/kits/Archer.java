7d81e3a47428dde45fabbc26e80cdbc7e778375b
ddilley926 <dustin.diolley@hotmail.com>
2015-03-20 09:02:56 -0700

    More to archer

--- src/com/dustindilley/KitPvP/kits/Archer.java
+++ src/com/dustindilley/KitPvP/kits/Archer.java
@@ -5,8 +5,10 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import com.dustindilley.KitPvP.Main;
 
@@ -29,16 +31,32 @@ public class Archer implements CommandExecutor{
 					
 					
 					//TODO FINISH ADDING THE ON DEATH EVENT IN THE EVENTS CLASS
-				
+			ItemStack ArcherBow = new ItemStack(Material.BOW);
+			ArcherBow.addEnchantment(Enchantment.ARROW_INFINITE, 2);
+			ArcherBow.addEnchantment(Enchantment.ARROW_DAMAGE, 3 );
+			
+			
+			ItemMeta ArcherMeta = ArcherBow.getItemMeta();
+			ArcherMeta.setDisplayName(ChatColor.GOLD + "ArcherBow");
+			
+			
 			p.sendMessage(ChatColor.RED + "You have recieved the Archer kit!");
+			p.sendMessage(ChatColor.RED + "Now go have some fun!");
+			
+				
+			
 			
 			p.getInventory().setHelmet(new ItemStack(Material.CHAINMAIL_HELMET));
-			p.getInventory().setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
-			p.getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
-			p.getInventory().setBoots(new ItemStack(Material.IRON_BOOTS));
+			p.getInventory().setChestplate(new ItemStack(Material.CHAINMAIL_CHESTPLATE));
+			p.getInventory().setLeggings(new ItemStack(Material.CHAINMAIL_LEGGINGS));
+			p.getInventory().setBoots(new ItemStack(Material.CHAINMAIL_BOOTS));
 			
 			
+			p.getInventory().addItem(new ItemStack(Material.STONE_SWORD));
+			p.getInventory().addItem(ArcherBow);
 			
+			for(int i = 0; i<25; i++)
+				p.getInventory().addItem(new ItemStack(Material.MUSHROOM_SOUP));
 			
 		}
 		
