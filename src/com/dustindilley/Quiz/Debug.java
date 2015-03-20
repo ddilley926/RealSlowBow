97da7f09001cfb2c228167c72679d9287bae14d3
bradencohen <braden.cohen@yahoo.com>
2015-03-20 00:57:58 -0700

    Debugging

--- src/com/dustindilley/Quiz/Debug.java
+++ src/com/dustindilley/Quiz/Debug.java
@@ -1,12 +1,25 @@
 package com.dustindilley.Quiz;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class Debug {
+public class Debug extends JavaPlugin{
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel,
 			String[] args) {
+		
+		if(cmd.getName().equalsIgnoreCase("Heal")){
+			Player penis = (Player) sender;
+			penis.setHealth(20);
+			penis.setHunger(20);
+			penis.sendMessage(ChatColor.GOLD + "You have been fully healed.");
+			
+		}
+		
+		
 				return false;
 }
 
