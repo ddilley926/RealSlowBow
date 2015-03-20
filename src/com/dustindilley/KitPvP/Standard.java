626db12cd1e3c322053855d92b177cb5020825b6
bradencohen <braden.cohen@yahoo.com>
2015-03-19 19:14:40 -0700

    Starting Debugging KitPvP

--- src/com/dustindilley/KitPvP/Standard.java
+++ src/com/dustindilley/KitPvP/Standard.java
@@ -4,7 +4,6 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -16,10 +15,6 @@ public class Standard implements CommandExecutor{
 		if(cmd.getName().equalsIgnoreCase("Standard")){
 			
 		Player p = (Player) sender;
-		
-	ItemStack Leggings = new ItemStack(Material.IRON_LEGGINGS);
-	Leggings.addEnchantment(Enchantment.DAMAGE_ALL, 3);
-		
 		
 		p.getInventory().clear();
 		p.getInventory().addItem(new ItemStack(Material.IRON_SWORD));
